de354efb779303d446203ae837acc91bf2033284
SimonS2019 <shentiexiong@gmail.com>
2025-02-19 16:16:20 +1300

    feat: update app component to include standalone components and ngb-alert for warnings

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,31 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NgbAlertModule } from '@ng-bootstrap/ng-bootstrap';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { SkillsComponent } from './skills/skills.component';
+import { CurriculumVitaeComponent } from './curriculum-vitae/curriculum-vitae.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { ReferencesComponent } from './references/references.component';
+import { ContactComponent } from './contact/contact.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,NgbAlertModule],
+  imports: [
+    RouterOutlet,
+    NgbAlertModule,
+    HeaderComponent,
+    HomeComponent,
+    AboutComponent,
+    SkillsComponent,
+    CurriculumVitaeComponent,
+    ProjectsComponent,
+    ReferencesComponent,
+    ContactComponent
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css',
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {}
