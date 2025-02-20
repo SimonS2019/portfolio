b5ac96ca61160181678443e098099d808cb7f0aa
SimonS2019 <shentiexiong@gmail.com>
2025-02-21 09:23:23 +1300

    feat: update about component to fetch and display personal information from data service

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,12 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { DataService } from '../services/data.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [],
+  imports: [HttpClientModule, CommonModule],
   templateUrl: './about.component.html',
-  styleUrl: './about.component.css'
+  styleUrl: './about.component.css',
+  providers: [DataService],
 })
-export class AboutComponent {
+export class AboutComponent implements OnInit {
+  aboutMe: any = {
+    coreSkills: [],
+    tools: [],
+    deployment: [],
+    description: '',
+    imagePath: '',
+    additionalInfo: '',
+  };
 
+  constructor(private dataService: DataService) {}
+
+  ngOnInit(): void {
+    this.dataService.getBasicInfo().subscribe({
+      next: (data) => {
+        this.aboutMe = data.aboutMe;
+      },
+      error: (error) => {
+        console.error('Error fetching data', error);
+      },
+    });
+  }
 }
