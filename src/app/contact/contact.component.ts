908ff57f563a6cf9947cdb5cb98824f01209c068
SimonS2019 <shentiexiong@gmail.com>
2025-02-21 09:07:23 +1300

    feat: refactor contact component to improve social links rendering and data fetching

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,25 +1,25 @@
 import { HttpClientModule } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { DataService } from '../services/data.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [HttpClientModule],
+  imports: [HttpClientModule,CommonModule],
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.css'],
   providers: [DataService],
 })
 export class ContactComponent implements OnInit {
-  contactInfo: any;
+  contactInfo: any = {};
 
   constructor(private dataService: DataService) {}
 
   ngOnInit(): void {
     this.dataService.getBasicInfo().subscribe({
       next: (data) => {
-        console.log(data);
-        this.contactInfo = data.basicInfo;
+        this.contactInfo = data;
       },
       error: (error) => {
         console.error('Error fetching data', error);
