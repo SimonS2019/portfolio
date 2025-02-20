060eb6d21cdca7ff31b05d97a71453f69387b7c9
SimonS2019 <shentiexiong@gmail.com>
2025-02-21 12:27:26 +1300

    feat: add Project interface and ProjectTag enum to enhance project data structure

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -2,6 +2,58 @@ import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { DataService } from '../services/data.service';
 
+// Define project tags enum
+enum ProjectTag {
+  FrontEnd = 'Front-End', // Projects focused on front-end development
+  BackEnd = 'Back-End', // Projects focused on back-end development
+  FullStack = 'Full-Stack', // Projects involving both front-end and back-end development
+  Angular = 'Angular', // Projects using Angular framework
+  React = 'React', // Projects using React library
+  // Vue = 'Vue', // Projects using Vue.js framework
+  Node = 'Node.js', // Projects using Node.js for server-side development
+  Express = 'Express', // Projects using Express framework for building APIs
+  DotNetCore = '.NET Core', // Projects using .NET Core framework
+  Azure = 'Azure', // Projects using Microsoft Azure cloud services
+  Docker = 'Docker', // Projects involving Docker for containerization
+  Firebase = 'Firebase', // Projects using Firebase platform
+  GoogleCloud = 'Google Cloud', // Projects using Google Cloud services
+  // K8s = 'Kubernetes', // Projects using Kubernetes for container orchestration
+  TypeScript = 'TypeScript', // Projects using TypeScript programming language
+  HTML5 = 'HTML5', // Projects using HTML5 for web structure
+  CSS3 = 'CSS3', // Projects using CSS3 for styling
+  SASS = 'SASS/SCSS', // Projects using SASS/SCSS for advanced styling
+  JavaScript = 'JavaScript', // Projects using JavaScript for front-end scripting
+  Bootstrap = 'Bootstrap', // Projects using Bootstrap framework for responsive design
+  SQL = 'SQL', // Projects using SQL for database management
+  MongoDB = 'MongoDB', // Projects using MongoDB for NoSQL database management
+  Game = 'Game', // Projects focused on game development
+  Sandbox = 'Sandbox', // Projects involving sandbox or open-world features
+  WebsiteBuilders = 'Website Builders', // Projects using website builders like Duda, Wix, WordPress
+  Other = 'Other', // Projects that do not fall into any of the above categories
+}
+
+interface Project {
+  title: string;
+  description: string;
+  link: string;
+  image: string;
+  technologies: string[];
+  demoLink?: string;
+  repoLink?: string;
+  role: string;
+  duration: string; // e.g., "January 2023 - March 2023"
+  features: string[];
+  challenges: string[];
+  learnings: string[];
+  feedback?: string[];
+  type: string; // e.g., "Personal", "Freelance", "Open-Source"
+  awards?: string[];
+  videoLink?: string;
+  contribution?: number; // Percentage of your contribution
+  tags: ProjectTag[];
+  downloadLink?: string; // Link to download the application
+}
+
 @Component({
   selector: 'app-projects',
   standalone: true,
@@ -11,7 +63,7 @@ import { DataService } from '../services/data.service';
   providers: [DataService],
 })
 export class ProjectsComponent implements OnInit {
-  projects: any[] = [];
+  projects: Project[] = [];
 
   constructor(private dataService: DataService) {}
 
