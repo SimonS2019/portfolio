13f840b8707d2ef381216a43455dbd4b87da6633
SimonS2019 <shentiexiong@gmail.com>
2025-02-24 09:45:31 +1300

    feat: implement dynamic curriculum vitae component with data binding

--- src/app/curriculum-vitae/curriculum-vitae.component.ts
+++ src/app/curriculum-vitae/curriculum-vitae.component.ts
@@ -1,12 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { DataService } from '../services/data.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-curriculum-vitae',
   standalone: true,
-  imports: [],
+  imports: [HttpClientModule, CommonModule],
   templateUrl: './curriculum-vitae.component.html',
-  styleUrl: './curriculum-vitae.component.css'
+  styleUrl: './curriculum-vitae.component.css',
+  providers: [DataService],
 })
-export class CurriculumVitaeComponent {
+export class CurriculumVitaeComponent implements OnInit {
+  curriculumVitae: any = {
+    education: [],
+    experiences: [],
+    interests: [],
+  };
 
+  constructor(private dataService: DataService) {}
+
+  ngOnInit(): void {
+    this.dataService.getBasicInfo().subscribe({
+      next: (data) => {
+        this.curriculumVitae = data.curriculumVitae;
+      },
+      error: (error) => {
+        console.error('Error fetching data', error);
+      },
+    });
+  }
 }
